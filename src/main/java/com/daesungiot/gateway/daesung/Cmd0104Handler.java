bd95da6d344571c2d1b4f42cc3037199485a2cb7
Kim971202 <kimyohan971202@gmail.com>
2024-07-12 10:25:37 +0900

    한국 시간만 받아오게 수정

--- src/main/java/com/daesungiot/gateway/daesung/Cmd0104Handler.java
+++ src/main/java/com/daesungiot/gateway/daesung/Cmd0104Handler.java
@@ -29,7 +29,6 @@ public class Cmd0104Handler implements ResponseHandler {
     public BinaryMessage handle(ChannelHandlerContext ctx, String cseid, String dKey, BinaryMessage msg) {
         System.out.println("Cmd0104Handler -> handle CALLED");
         RemoteMessage req = (RemoteMessage) msg;
-        System.out.println("11111111111111111111111111111111111111111111111111111");
         Calendar cal = Calendar.getInstance();
         String wk = String.valueOf(cal.get(Calendar.DAY_OF_WEEK)-1);
         String dateString = sdf.format(cal.getTime());
@@ -38,29 +37,45 @@ public class Cmd0104Handler implements ResponseHandler {
             JSONObject jsonObject = new Gson().fromJson (new StringReader(new String(req.getBody())), JSONObject.class);
             String utcT = (String) jsonObject.get("utcT");
             System.out.println("utcT: " + utcT);
-            TimeZone tz = TimeZone.getDefault();
 
-            try {
-                int sign = 1;
-                if(utcT.charAt(0) == '-')
-                    sign = -1;
-                else if(utcT.charAt(0) == '+')
-                    sign = 1;
-                else
-                    new Exception("invaild format");
+//            TimeZone tz = TimeZone.getDefault();
 
-                int offsetMillis = (sign * Integer.parseInt(utcT.substring(1, 3))*60*60*1000) + (sign * Integer.parseInt(utcT.substring(4))*60*1000);
-                System.out.println("offsetMillis: " + offsetMillis);
-                tz.setRawOffset(offsetMillis);
-                System.out.println("tz.getRawOffset(): " + tz.getRawOffset());
-            } catch (Exception e) {
-                System.out.println(e);
-            }
+
+//            try {
+//                int sign = 1;
+//                if (utcT.charAt(0) == '-') {
+//                    sign = -1;
+//                } else if (utcT.charAt(0) == '+') {
+//                    sign = 1;
+//                } else {
+//                    throw new Exception("invalid format");
+//                }
+//
+//                int offsetMillis = (sign * Integer.parseInt(utcT.substring(1, 3))*60*60*1000) + (sign * Integer.parseInt(utcT.substring(4))*60*1000);
+//                System.out.println("offsetMillis: " + offsetMillis);
+//                tz.setRawOffset(offsetMillis);
+//                System.out.println("tz.getRawOffset(): " + tz.getRawOffset());
+//            } catch (Exception e) {
+//                System.out.println(e);
+//            }
+
+            // 한국 시간대 설정
+            TimeZone tz = TimeZone.getTimeZone("Asia/Seoul");
+
+            // 원하는 형식으로 날짜 포맷 지정
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+            sdf.setTimeZone(tz);
+
+            // 현재 한국 시간 출력
+            String koreanTime = sdf.format(cal.getTime());
+            System.out.println("Current Korean Time: " + koreanTime);
+
             cal = Calendar.getInstance(tz);
             wk = String.valueOf(cal.get(Calendar.DAY_OF_WEEK)-1);
             dateString = sdf.format(cal.getTime());
 
             System.out.println("wk: " + wk);
+            System.out.println("koreanTime: " + koreanTime);
             System.out.println("dateString: " + dateString);
 
         } catch (Exception e1) {
